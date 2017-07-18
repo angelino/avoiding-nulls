29d528eb4c978fe5b2f315ccd8ec95c0e08b9128
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2017-07-18 17:35:30 -0300

    Add potential null pointer exception on client code

--- src/Application.java
+++ src/Application.java
@@ -6,10 +6,9 @@ public class Application {
     public static void main(String[] args) {
         UserService userService = new InMemoryUserService();
 
-        Long userId = 1L;
+        Long userId = 3L;
 
         User user = userService.findUserById(userId);
-
-        System.out.println(user);
+        System.out.println(user.getName());
     }
 }
