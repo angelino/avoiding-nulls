a96ab7017972f9053b702fb8561eabbc73b5e649
Lucas Angelino dos Santos <lucas.santos@synchro.com.br>
2017-07-18 17:33:23 -0300

    Fix client code to not send null as user id

--- src/Application.java
+++ src/Application.java
@@ -6,7 +6,7 @@ public class Application {
     public static void main(String[] args) {
         UserService userService = new InMemoryUserService();
 
-        Long userId = null;
+        Long userId = 1L;
 
         User user = userService.findUserById(userId);
 
